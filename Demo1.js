295c310b1d2fdaa1126cc6e869b3868a9d9391b5
unknown <iamnilesh4vision@gmail.com>
2026-08-15 15:43:07 +0530

    commit

--- Demo1.js
+++ Demo1.js
@@ -5,15 +5,4 @@ console.log("Practice Github");
 
 console.log("Practice Github");
 
-console.log("Hello");
-
-console.log("Nilesh made changes");
-console.log("Nilesh made changes");
-console.log("Nilesh made changes");
-
-console.log("Divya made changes");
-console.log("Divya made changes");
-
-console.log("15 august");
-
-console.log("Work after Nilesh push by Divya");
+console.log("practice for rebase master or merge master.");
